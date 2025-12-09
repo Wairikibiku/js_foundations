92e0a8fc9bc679cf501b35af2f962b7fa1a6fc0b
Wairikibiku <denniswairikibiku@gmail.com>
2025-12-09 11:28:34 +0300

    intial commit

---
+++ 1_variable.js
@@ -0,0 +1,12 @@
+let age = 23;
+let name = "deno";
+
+console.log("My name is " + name + ", I am " + age);
+
+// Template literal (correct)
+console.log(`In 10 years I will be ${age + 10} years old`);
+
+age = age + 10;
+console.log("In the next ten years I will be " + age);
+
+
---
+++ 3-Operators.js
@@ -0,0 +1,9 @@
+//1 Arithimetic operators
+//addition
+let a=4;
+let b=2;
+console.log(a+b); 
+// subtraction
+let x =5;
+let y =2;
+console.log(x-y);
