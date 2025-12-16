f87a505ea5270c7db5fe672a5dac85a33733c739
Wairikibiku <denniswairikibiku@gmail.com>
2025-12-16 11:14:44 +0300

    greeting function and sum function

---
+++ 10-excercise.js
@@ -0,0 +1,11 @@
+// a greeting function
+function greet(name) {
+   console.log("Hello, " + name + "!"); //name serves as the parameter 
+}
+greet("deno");
+
+ //sum of two numbers
+ function sum(a, b) {
+    return a + b;
+ }
+ console.log(sum(3, 5));
